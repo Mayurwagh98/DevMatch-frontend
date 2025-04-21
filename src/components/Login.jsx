2d3f7bcd72939e795de6bc30a195488fa88ead80
mayur wagh <mswagh98@gmail.com>
2025-04-21 17:39:51 +0530

    feat(login): add login form with email and password validation
    This commit introduces a new login form component with fields for email and password. The form includes validation rules to ensure the email is valid and the password meets complexity requirements. Additionally, the axios library has been added as a dependency to support future API integration.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,91 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <div className="w-[40%] flex justify-center items-center mx-auto my-6">
+        <div className="card bg-neutral text-neutral-content w-[80%] min-h-[300px]">
+          <div className="card-body items-center text-center">
+            <h1 className="text-2xl font-bold">Login</h1>
+            {/* email */}
+            <label className="input validator my-2">
+              <svg
+                className="h-[1em] opacity-50"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                </g>
+              </svg>
+              <input type="email" placeholder="mail@site.com" required />
+            </label>
+            <div className="validator-hint hidden">
+              Enter valid email address
+            </div>
+            {/* password */}
+            <label className="input validator">
+              <svg
+                className="h-[1em] opacity-50"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                  <circle
+                    cx="16.5"
+                    cy="7.5"
+                    r=".5"
+                    fill="currentColor"
+                  ></circle>
+                </g>
+              </svg>
+              <input
+                type="password"
+                required
+                placeholder="Password"
+                minlength="8"
+                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+              />
+            </label>
+            <p className="validator-hint hidden">
+              Must be more than 8 characters, including
+              <br />
+              At least one number <br />
+              At least one lowercase letter <br />
+              At least one uppercase letter
+            </p>
+            <div className="card-actions justify-end my-3">
+              <button className="btn btn-primary">Login</button>
+            </div>
+            <div className="divider my-2">OR</div>
+            <div className="card-actions justify-end">
+              <p>
+                Don't have an account?{" "}
+                <Link to={"/signup"} className="text-blue-400">
+                  SignUp
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
