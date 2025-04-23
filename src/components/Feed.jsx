c53ae1f150ecb4283897fd64aafce4ae4f2986f7
mayur wagh <mswagh98@gmail.com>
2025-04-23 15:47:19 +0530

    feat(Feed): add UserCard component to display feed data
    Integrate UserCard component into the Feed to display user details from the feed state. This enhances the user interface by showing relevant information in a structured manner.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,19 @@
+import { useSelector } from "react-redux";
 import useGetMyFeed from "../hooks/useGetMyFeed";
+import UserCard from "./UserCard";
 
 const Feed = () => {
+  const { feed } = useSelector((state) => state.feed);
   useGetMyFeed();
-  return <>feed</>;
+  return (
+    <>
+      {feed && (
+        <div className="flex justify-center items-center mt-10">
+          <UserCard user={feed[0]} />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Feed;
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,28 @@
+const UserCard = ({ user }) => {
+  const { firstName, lastName, age, gender, photoUrl, about } = user;
+
+  return (
+    user && (
+      <div className="card bg-base-300 w-96 shadow-sm h-[30rem]">
+        <figure>
+          <img
+            src={photoUrl}
+            alt="image"
+            className="w-full h-full object-cover"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{firstName + " " + lastName}</h2>
+          <p>{age + " " + gender}</p>
+          <p>{about}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Ignore</button>
+            <button className="btn btn-primary">Interested</button>
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default UserCard;
