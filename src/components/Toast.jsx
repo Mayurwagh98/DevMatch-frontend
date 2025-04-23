81c6cb92b2733a423d8ad20349e51208400b4de1
mayur wagh <mswagh98@gmail.com>
2025-04-23 17:49:19 +0530

    feat(UpdateProfile): add toast notification for profile update success
    Introduce a Toast component to display a success message after a successful profile update. The message is displayed for 3 seconds before disappearing. This improves user feedback and enhances the user experience.

---
+++ src/components/Toast.jsx
@@ -0,0 +1,10 @@
+const Toast = ({ text }) => {
+  return (
+    <div className="toast toast-top toast-center">
+      <div className="alert alert-success">
+        <span>{text}</span>
+      </div>
+    </div>
+  );
+};
+export default Toast;
--- src/components/UpdateProfile.jsx
+++ src/components/UpdateProfile.jsx
@@ -4,9 +4,11 @@ import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch } from "react-redux";
 import { addUser } from "../redux/authSlices/loginSlice";
+import Toast from "./Toast";
 
 const UpdateProfile = ({ user: userData }) => {
   const dispatch = useDispatch();
+  const [profileMessage, setProfileMessage] = useState("");
   const [profileUpdateData, setProfileUpdateData] = useState({
     firstName: userData?.firstName,
     lastName: userData?.lastName,
@@ -28,83 +30,88 @@ const UpdateProfile = ({ user: userData }) => {
       );
       if (data.success) {
         dispatch(addUser(data.loggedInUser));
+        setProfileMessage(data.message);
       }
+      setTimeout(() => {
+        setProfileMessage("");
+      }, 3000);
     } catch (error) {
       console.log("error:", error);
     }
   };
 
   return (
-    <div className="flex mx-3 w-full">
-      <div className="flex justify-center items-center mx-auto my-24">
-        <div className="card bg-neutral text-neutral-content min-h-[300px] w-96">
-          <div className="card-body items-center text-center">
-            <h1 className="text-2xl font-bold">Profile</h1>
-            {/* firstname */}
-            <label className="input validator my-2">
-              <input
-                type="text"
-                placeholder="Enter your firstname"
-                required
-                value={firstName}
-                onChange={(e) =>
-                  setProfileUpdateData({
-                    ...profileUpdateData,
-                    firstName: e.target.value,
-                  })
-                }
-              />
-            </label>
-            {/* last name */}
-            <label className="input validator my-2">
-              <input
-                type="text"
-                placeholder="Enter your lastname"
-                required
-                value={lastName}
-                onChange={(e) =>
-                  setProfileUpdateData({
-                    ...profileUpdateData,
-                    lastName: e.target.value,
-                  })
-                }
-              />
-            </label>
-            {/* photo url */}
-            <label className="input validator my-2">
-              <input
-                type="text"
-                placeholder="Enter your photourl"
-                required
-                value={photoUrl}
-                onChange={(e) =>
-                  setProfileUpdateData({
-                    ...profileUpdateData,
-                    photoUrl: e.target.value,
-                  })
-                }
-              />
-            </label>
-            {/* age */}
-            <label className="input validator my-2">
-              <input
-                type="number"
-                placeholder="Enter your age"
-                required
-                value={age}
-                onChange={(e) =>
-                  setProfileUpdateData({
-                    ...profileUpdateData,
-                    age: e.target.value,
-                  })
-                }
-              />
-            </label>
-            <label className="input validator my-2">
-              <input
-                type="text"
-                placeholder="Enter about you"
-                required
+    <>
+      {profileMessage && <Toast text={profileMessage} />}
+      <div className="flex mx-3 w-full">
+        <div className="flex justify-center items-center mx-auto my-24">
+          <div className="card bg-neutral text-neutral-content min-h-[300px] w-96">
+            <div className="card-body items-center text-center">
+              <h1 className="text-2xl font-bold">Profile</h1>
+              {/* firstname */}
+              <label className="input validator my-2">
+                <input
+                  type="text"
+                  placeholder="Enter your firstname"
+                  required
+                  value={firstName}
+                  onChange={(e) =>
+                    setProfileUpdateData({
+                      ...profileUpdateData,
+                      firstName: e.target.value,
+                    })
+                  }
+                />
+              </label>
+              {/* last name */}
+              <label className="input validator my-2">
+                <input
+                  type="text"
+                  placeholder="Enter your lastname"
+                  required
+                  value={lastName}
+                  onChange={(e) =>
+                    setProfileUpdateData({
+                      ...profileUpdateData,
+                      lastName: e.target.value,
+                    })
+                  }
+                />
+              </label>
+              {/* photo url */}
+              <label className="input validator my-2">
+                <input
+                  type="text"
+                  placeholder="Enter your photourl"
+                  required
+                  value={photoUrl}
+                  onChange={(e) =>
+                    setProfileUpdateData({
+                      ...profileUpdateData,
+                      photoUrl: e.target.value,
+                    })
+                  }
+                />
+              </label>
+              {/* age */}
+              <label className="input validator my-2">
+                <input
+                  type="number"
+                  placeholder="Enter your age"
+                  required
+                  value={age}
+                  onChange={(e) =>
+                    setProfileUpdateData({
+                      ...profileUpdateData,
+                      age: e.target.value,
+                    })
+                  }
+                />
+              </label>
+
+              <textarea
+                className="textarea"
+                placeholder="something about you"
                 value={about}
                 onChange={(e) =>
                   setProfileUpdateData({
@@ -112,39 +119,38 @@ const UpdateProfile = ({ user: userData }) => {
                     about: e.target.value,
                   })
                 }
-              />
-            </label>
-            {/* gender */}
+              ></textarea>
 
-            <label className="input validator my-2">
-              <input
-                type="text"
-                placeholder="Enter your skills"
-                required
-                value={skills}
-                onChange={(e) =>
-                  setProfileUpdateData({
-                    ...profileUpdateData,
-                    skills: e.target.value,
-                  })
-                }
-              />
-            </label>
-            <div className="card-actions justify-end my-3">
-              <button className="btn btn-primary" onClick={updateProfile}>
-                Save
-              </button>
+              <label className="input validator my-2">
+                <input
+                  type="text"
+                  placeholder="Enter your skills"
+                  required
+                  value={skills}
+                  onChange={(e) =>
+                    setProfileUpdateData({
+                      ...profileUpdateData,
+                      skills: e.target.value,
+                    })
+                  }
+                />
+              </label>
+              <div className="card-actions justify-end my-3">
+                <button className="btn btn-primary" onClick={updateProfile}>
+                  Save
+                </button>
+              </div>
             </div>
           </div>
         </div>
+        <div className="mr-[150px]">
+          <p className="mt-10">Your profile will look like this:</p>
+          <UserCard
+            user={{ firstName, lastName, age, photoUrl, about, skills }}
+          />
+        </div>
       </div>
-      <div className="mr-[150px]">
-        <p className="mt-10">Your profile will look like this:</p>
-        <UserCard
-          user={{ firstName, lastName, age, photoUrl, about, skills }}
-        />
-      </div>
-    </div>
+    </>
   );
 };
 
