d022ce57cb301ce187b951dea87de01eba2b0d83
mayur wagh <mswagh98@gmail.com>
2025-04-24 16:21:30 +0530

    feat(connections): add connections slice and integrate with useGetConnections hook
    Introduce a new Redux slice for managing user connections, including actions for requesting and adding connections. Integrate this slice with the  hook to fetch and store connections data in the Redux store.

--- src/hooks/useGetConnections.js
+++ src/hooks/useGetConnections.js
@@ -1,17 +1,26 @@
 import { useEffect } from "react";
 import { BASE_URL } from "../utils/constants";
 import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addConnections,
+  requestConnnections,
+} from "../redux/connections/connectionsSlice";
 
 const useGetConnections = () => {
+  const dispatch = useDispatch();
+  const { connections } = useSelector((state) => state.connections);
   useEffect(() => {
+    if (connections) return;
     getMyConnections();
   }, []);
   const getMyConnections = async () => {
+    dispatch(requestConnnections());
     try {
       const { data } = await axios.get(BASE_URL + "/user/my_connections", {
         withCredentials: true,
       });
-      console.log("data:", data);
+      dispatch(addConnections(data));
     } catch (error) {
       console.log("error:", error);
     }
--- src/redux/appStore.js
+++ src/redux/appStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./authSlices/loginSlice";
 import feedReducer from "./feedSlices/feedSlice";
+import connectionsReducer from "./connections/connectionsSlice";
 
 const appStore = configureStore({
   reducer: {
     user: userReducer,
     feed: feedReducer,
+    connections: connectionsReducer,
   },
 });
 
