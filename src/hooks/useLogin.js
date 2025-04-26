8065ddf9c4cc1f09108d7aea569f3ff419e3a87f
mayur wagh <mswagh98@gmail.com>
2025-04-26 14:56:31 +0530

    refactor: use BASE_URL constant for API endpoints
    Replace hardcoded API URLs with the BASE_URL constant in useSignup and useLogin hooks to improve maintainability and reduce duplication

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -7,6 +7,7 @@ import {
 } from "../redux/authSlices/loginSlice";
 import { useNavigate } from "react-router-dom";
 import errorHandler from "../helpers/errorHandler";
+import { BASE_URL } from "../utils/constants";
 
 const useLogin = () => {
   const dispatch = useDispatch();
@@ -14,11 +15,9 @@ const useLogin = () => {
   const login = async (loginData) => {
     try {
       dispatch(requestLogin());
-      const { data } = await axios.post(
-        "http://localhost:8000/auth/login",
-        loginData,
-        { withCredentials: true }
-      );
+      const { data } = await axios.post(BASE_URL + "/auth/login", loginData, {
+        withCredentials: true,
+      });
       dispatch(addUser(data.sanitizedUser));
       if (data.success) {
         navigate("/");
