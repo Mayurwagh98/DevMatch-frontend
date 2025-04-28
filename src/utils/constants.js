7d883d5695329a896320f53f1c2f8a5898c87549
mayur wagh <mswagh98@gmail.com>
2025-04-28 13:13:55 +0530

    refactor(constants): update BASE_URL to dynamically set based on hostname
    Simplify the BASE_URL configuration by dynamically setting it based on the hostname, allowing for easier local development and production deployment.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = "http://localhost:8000";
-export const BASE_URL = "http://13.51.47.215/api";
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:8000" : "/api";
