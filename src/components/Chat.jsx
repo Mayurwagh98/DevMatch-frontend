14c2693035b5970cd019b517306a5b33ab9d7a85
mayur wagh <mswagh98@gmail.com>
2025-04-30 15:57:22 +0530

    feat(chat): add chat input and message state management
    Introduce state management for chat messages and a new message input field. This allows users to type and send messages within the chat interface. The input field is styled and includes validation to trim whitespace.

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
 const Chat = () => {
-  return <div>chat</div>;
+  const [chatMsg, setChatMsg] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  return (
+    <div className="border border-teal-600 w-[50%] h-[60vh] mx-auto my-[6%] rounded-md flex flex-col justify-between">
+      <div className="border-b border-b-teal-400 py-2 px-3">
+        <h2 className="text-xl">Chat with Mayur</h2>
+      </div>
+      <form
+        className="w-full py-3 mx-auto"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <input
+          type="text"
+          placeholder="Type your message"
+          className="border border-purple-500 rounded-md p-2 mx-2 w-[85%] text-sm"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value.trim())}
+        />
+        <button className="bg-purple-600 text-white rounded-md p-2 mx-2">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Chat;
