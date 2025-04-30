5f1dc480d885a32dd9d6db3c5734ce8f4fd3e79b
mayur wagh <mswagh98@gmail.com>
2025-04-30 16:27:47 +0530

    feat(socket): add socket.io-client integration for chat functionality
    This commit introduces socket.io-client to enable real-time communication in the chat component. The  utility function is added to establish a socket connection, and the  component is updated to use this function to join a chat room based on user and receiver IDs. The necessary dependencies are also added to package.json and package-lock.json.

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { createSocketConnection } from "../utils/socket";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 const Chat = () => {
+  const { receiverId } = useParams();
+  const { userData } = useSelector((state) => state.user);
   const [chatMsg, setChatMsg] = useState([]);
   const [newMessage, setNewMessage] = useState("");
+
+  useEffect(() => {
+    startSocket();
+  }, [userData]);
+
+  const startSocket = () => {
+    if (!userData) return;
+    const socket = createSocketConnection();
+
+    socket.emit("joinChat", {
+      firstName: userData?.firstName,
+      userId: userData?._id,
+      receiverId: receiverId,
+    });
+  };
+
   return (
     <div className="border border-teal-600 w-[50%] h-[60vh] mx-auto my-[6%] rounded-md flex flex-col justify-between">
       <div className="border-b border-b-teal-400 py-2 px-3">
